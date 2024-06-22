600c376e216b9c8ec748ee4aa6b538817bc528ea
Tuan Kieu <joseph.kieu@bluebelt.asia>
2024-06-22 12:54:41 +0700

    test #time 1h #comment fix smart commit #resolve

--- git/smart_commit.sh
+++ git/smart_commit.sh
@@ -3,7 +3,7 @@
 read -p "Enter the issue key (e.g., JRA-123): " issue_key
 read -p "Enter the time spent (e.g., 2d 5h): " time_spent
 read -p "Enter the comment: " comment
-commit_message="$issue_key #time $time_spent #comment $comment #resolve"
+commit_message="$issue_key #time $time_spent #comment $comment"
 git commit -m "$commit_message"
 if [ $? -eq 0 ]; then
     echo "Commit successful!"
